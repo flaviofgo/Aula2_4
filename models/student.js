92c52e7392fec79ed4f9b1e7f2f99f762c236b40
FlavioOliveira <fgo.flavio@gmail.com>
2020-09-03 11:17:08 -0300

    Adiciona cometários no modelo

--- models/student.js
+++ models/student.js
@@ -17,6 +17,7 @@ const studentSchema = mongoose.Schema({
     type: Number,
     required: true,
     //Valida se a nota inserida e' menor que zero
+    //valida que a nota não seja menor que zero
     validate(value) {
       if (value < 0) throw new Error('Valor negativo para nota');
     },
